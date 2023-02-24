a2a572c8a2d1288da23789ecae426964552af9dc
unknown <dawidw22@gmail.com>
2023-02-24 14:36:56 +0100

    feat: finish tabs and features section

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const btn = document.querySelectorAll("[data-tab-button]");
 const tabs = document.querySelectorAll("[data-tab-content]");
+
 btn.forEach((button) => {
   button.addEventListener("click", (e) => {
     tabs.forEach((content) => {
       content.classList.remove("current");
     });
+    btn.forEach((b) => {
+      b.classList.remove("active");
+    });
 
     const element = document.getElementById(e.target.dataset.tabButton);
     element.classList.add("current");
+    button.classList.add("active");
   });
 });
